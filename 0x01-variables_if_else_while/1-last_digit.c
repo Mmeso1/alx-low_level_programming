ad94c5719d2e3c93d2bdcb26377a49542716142b
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-11 15:26:20 -0700

    Identifying and comparing last digits of randomly generated numbers

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description: Identifying and comparing last digits
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int rem = n % 10;
+
+	printf("Last digit is");
+
+	if (rem > 5)
+	{
+		printf(" %i and is greater than 5\n", rem);
+	}
+	else if (rem == 0)
+	{
+		printf(" %i and is 0\n", rem);
+	}
+	else
+	{
+		printf(" %i and is less than 6 and not 0\n", rem);
+	}
+	return (0);
+}
