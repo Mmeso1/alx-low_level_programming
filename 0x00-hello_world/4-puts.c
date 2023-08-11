ce960455dd8ed68ccbc6d8ac22e1a6561b4d543e
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-10 21:48:17 -0700

    C program to output a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description : It prints a string
+ * Return: nothing
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
