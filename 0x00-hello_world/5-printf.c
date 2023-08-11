cf8f4c1376b8baf520ff13be06feb04bee227590
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-10 21:58:44 -0700

    To print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- This is the main function
+ * Description - To print a string that ends with a new line
+ * Return - It returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
