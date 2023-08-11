7a2d107b5b7c21df5a294cb0347655b4f2adf25b
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-10 22:38:03 -0700

    C program that prints a string to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - This is the entry point
+ * Description: Prints a specific character to standard error
+ * Return: It will return 1 signifying an error
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
