1ceb68fa57ea18e38fea97eaadf660e99a28aac4
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-11 14:54:29 -0700

    Identify negative and positive numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description: To identify positive and negative and zero digits
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero", n);
+	}
+	else
+	{
+		printf("%i is negative", n);
+	}
+
+	return (0);
+}
