084f3e7ec0f6e516e0e66d58bd1617789eacbac4
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-11 15:42:54 -0700

    Identifying and comparing last digits of randomly generated numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,19 +17,19 @@ int main(void)
 	/* your code goes there */
 	rem = n % 10;
 
-	printf("Last digit is");
+	printf("Last digit of");
 
 	if (rem > 5)
 	{
-		printf(" %i and is greater than 5\n", rem);
+		printf(" %i is %i and is greater than 5\n", n, rem);
 	}
 	else if (rem == 0)
 	{
-		printf(" %i and is 0\n", rem);
+		printf(" %i is %i and is 0\n", n, rem);
 	}
 	else
 	{
-		printf(" %i and is less than 6 and not 0\n", rem);
+		printf(" %i is %i and is less than 6 and not 0\n", n, rem);
 	}
 	return (0);
 }
