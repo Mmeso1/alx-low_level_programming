266606d44ce7bcd57d4d0f3463b0b6eb9ccf1866
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-10 22:14:12 -0700

    Print sizes of types on various computers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description - Prints size of the various types on the computer
+ * Return - It returns 0
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
