d2a149e6a27111e4b2eafbe374f47fab4b7e6c30
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-11 17:10:04 -0700

    Print hexadecimal digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description: To print numbers in hexadecimal
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + 'O');
+	}
+
+	for (n = 'a'; n <= 'f'; i++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
