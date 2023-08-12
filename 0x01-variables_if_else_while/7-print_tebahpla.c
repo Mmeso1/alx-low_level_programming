b2e899f71b2a25cf0ecd089293ba9844568f8cc5
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-11 17:01:23 -0700

    Print letters in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description: To print out letters in reverse order
+ * Return: 0 for success :)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
