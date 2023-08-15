d7016c78d30a72b1973f8c0a2713f441cd4c2716
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-15 12:42:17 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,11 +16,11 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = -n;
+		n = -(long)n;
 	}
 
 	last_digit = n % 10;
-	_putchar('0' + last_digit);
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
