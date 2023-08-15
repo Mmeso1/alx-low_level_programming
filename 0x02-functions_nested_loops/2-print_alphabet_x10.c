5bb85f5360060084b639693dc118a10985c78bcb
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-15 07:44:57 -0700

    Print lowercase 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Function name
+ * Description: Print lowercase 10x
+ * Return: Zero
+ */
+void print_alphabet_x10(void)
+{
+	int i = 97;
+	int count = 10;
+
+	while (count <= 10)
+	{
+		for (i; i <= 122; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+}
