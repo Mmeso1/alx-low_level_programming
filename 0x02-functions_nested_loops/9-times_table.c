eff423393a5b6c314295466a787ca2a62c0273c4
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-15 23:19:50 -0700

    Print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,23 @@ void times_table(void)
 		{
 			product = i * j;
 
-			if (j != 0)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-
-			if (product < 10)
-				_putchar(' ');
+			if (j == 0)
+			{
+				product = i * j;
+			}
 			else
-				_putchar(product / 10 + '0');
-			_putchar(product % 10 + '0');
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (product < 10)
+					_putchar(' ');
+				else
+					_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
 		}
+
 		_putchar('\n');
 	}
 }
