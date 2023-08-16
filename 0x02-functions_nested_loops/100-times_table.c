314c735b55479806154d240f334e9ed5fd6c4b9e
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2023-08-16 11:19:52 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,47 +8,40 @@
 */
 void print_times_table(int n)
 {
-	if (n >= 0 && n <= 15)
-	{
-		int i, j, product;
+    if (n >= 0 && n <= 15)
+    {
+        int i, j, product;
 
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				product = i * j;
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                product = i * j;
 
-				if (j == 0)
-					_putchar(i + '0');
+                if (j == 0)
+                {
+                    _putchar(product + '0');
+                }
+                else
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    
+                    if (product < 10)
+                    {
+                        _putchar(' ');
+                    }
+                    else if (product < 100)
+                    {
+                        _putchar(' ');
+                    }
 
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					
-					if (product < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (product < 100)
-					{
-						_putchar(' ');
-					}
-				}
-
-				if (product < 10)
-				{
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-				}
-				_putchar(product % 10 + '0');
-			}
-			_putchar('\n');
-		}
-	}
+                    _putchar(product / 100 + '0');
+                    _putchar((product / 10) % 10 + '0');
+                    _putchar(product % 10 + '0');
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
