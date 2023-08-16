3a4b86b7a7ce05a71ba8913c72dac513ea3d0297
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2023-08-16 08:33:44 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,70 +2,43 @@
 #include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
+ * print_number - Print a number using _putchar
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n >= 100)
+        _putchar(n / 100 + '0');
+    if (n >= 10)
+        _putchar((n / 10) % 10 + '0');
+    _putchar(n % 10 + '0');
+}
+
+/**
+ * print_to_98 - Print natural numbers from n to 98
  * @n: The starting number
- * Description: This function prints all natural numbers from the given
- * starting number @n up to 98, separated by a comma and a space.
- *
- * Return: void
  */
 void print_to_98(int n)
 {
-	int i = n;
+    int i = n;
 
-	if (n < 98 || n == 98)
-	{
-		for (; i <= 98; i++)
-		{
-			 if (i < 10)
-			 {
-				 _putchar(i + '0');
-			 }
-			 else if (i < 100)
-			 {
-				 _putchar(i / 10 + '0');
-				 _putchar(i % 10 + '0');
-			 }
-			 else
-			 {
-				 _putchar(i / 100 + '0');
-				 _putchar((i / 10) % 10 + '0');
-				 _putchar(i % 10 + '0');
-			 }
+    while (i != 98)
+    {
+        print_number(i);
+        _putchar(',');
+        _putchar(' ');
+        if (i < 98)
+            i++;
+        else
+            i--;
+    }
 
-			 if (i != 98)
-			 {
-				 _putchar(',');
-				 _putchar(' ');
-			 }
-		}
-	}
-	else
-	{
-		for (; i >= 98; i--)
-		{
-			 if (i < 10)
-			 {
-				 _putchar(i + '0');
-			 }
-			 else if (i < 100)
-			 {
-				 _putchar(i / 10 + '0');
-				 _putchar(i % 10 + '0');
-			 }
-			 else
-			 {
-				 _putchar(i / 100 + '0');
-				 _putchar((i / 10) % 10 + '0');
-				 _putchar(i % 10 + '0');
-			 }
-
-			 if (i != 98)
-			 {
-				 _putchar(',');
-				 _putchar(' ');
-			 }
-		}
-	}
-	_putchar('\n');
+    print_number(98);
+    _putchar('\n');
 }
