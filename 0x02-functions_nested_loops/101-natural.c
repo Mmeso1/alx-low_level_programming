05b5cf1555d6307c01bd7ccf55a3dce952735c2f
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-16 16:25:14 -0700

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 	int num, sum;
-
+	
 	sum = 0;
 	for (num = 0; num < 1024; num++)
 	{
-		 if (num % 3 == 0 || num % 5 == 0)
-		 {
-			 sum += num;
-		 }
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
 	}
-       	printf("%d\n", sum);
-       	return (0);
+	printf("%d\n", sum);
+	return (0);
 }
