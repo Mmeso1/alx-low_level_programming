03aae2e95c1fdc0750cdf47ae595482508d884d8
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2023-08-16 11:48:16 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,7 @@ product = i * j;
 
 if (product < 10)
 _putchar(' ');
+_putchar(' ');
 else if (product < 100)
 _putchar(product / 10 + '0');
 else
@@ -32,7 +33,7 @@ else
 _putchar(product / 100 + '0');
 _putchar((product / 10) % 10 + '0');
 }
-
+_putchar(' ');
 _putchar(product % 10 + '0');
 }
 
