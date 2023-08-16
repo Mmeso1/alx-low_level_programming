ce94d310f95a24b2dc5bcb37444a12502eba8010
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-16 04:38:48 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_fibonacci - Prints the first n Fibonacci numbers
+ * @n: The number of Fibonacci numbers to print
+ */
+void print_fibonacci(int n)
+{
+	int a = 1, b = 2, c;
+	int count = 2;
+
+	printf("%d, %d, ", a, b);
+
+	while (count < n)
+	{
+		c = a + b;
+		printf("%d", c);
+
+		if (count < n - 1)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = c;
+		count ++;
+	}
+	printf("\n");
+}
+
