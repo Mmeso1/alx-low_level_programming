843516687ff286eb0b9ae5d8c54dd44eadba65f8
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-17 22:39:15 -0700

    Print out numbers the way they are

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = _abs(n);
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
