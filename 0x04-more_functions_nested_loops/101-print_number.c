91d23956427b03fffbf584c2cbd3ddad358796a6
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-17 23:07:26 -0700

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,18 @@
 
 void print_number(int n)
 {
+	unsigned int k = n;
+
 	if (n < 0)
 	{
+		k = n * -1;
 		_putchar('-');
-		n = -n;
 	}
 
-	if (n / 10 != 0)
+	if (k / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(k);
 	}
 
-	_putchar(n % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
 }
