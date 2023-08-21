58b7579e9dfab4d5f0f2df2106cf6e4a46f01bd2
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-21 11:44:39 -0700

    Print the second half of argument

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Print the second half of the string
+ * @str: arg
+ */
+
+void puts_half(char *str)
+{
+	size_t i;
+	int midpoint = strlen(str) / 2;
+
+	for (i = midpoint; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putcahr('\n');
+}
