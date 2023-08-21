dbbc0c08efef3624ef9781135e269f1749709aa3
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-21 13:49:41 -0700

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,22 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
+	int r1 = result > INT_MAX / 10;
 
 	while (*s)
 	{
 		if (*s == '-')
 			sign = -sign;
 		else if (*s >= '0' && *s <= '9')
-			result = result * 10 + (*s - '0');
+		{
+			int digit = *s - '0';
+			if (sign == 1 && (r1 || r1 && digit > 7))
+				return INT_MAX;
+			if (sign == 1 && (r1 || r1 && digit > 8))
+				return INT_MIN;
+
+			result = result * 10 + digit;
+		}
 		else if (result != 0)
 			break;
 		s++;
