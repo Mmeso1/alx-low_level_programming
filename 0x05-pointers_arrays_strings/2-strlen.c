3eef2378dad40ec78523f95e320a3d920c5b2cdb
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-21 09:46:13 -0700

    Find the length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _strlen - Find the length of the arg
+ * @s: value whose length is to be checked
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
