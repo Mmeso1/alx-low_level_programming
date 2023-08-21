fbe0a623f49705a83f7e9601285a887fa041d298
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-21 12:30:28 -0700

    Print numbers in array consecutively

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Print numbers in array consecutively
+ * @a: array
+ * @n: number of items in array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i, ", a[i]);
+	}
+}
