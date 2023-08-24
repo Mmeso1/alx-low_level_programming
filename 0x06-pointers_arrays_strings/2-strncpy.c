b21014801c90da9c496ea405e5f01093d762c50d
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-24 08:05:50 -0700

    Copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy : copy a string from a src to a dest
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The copy limit
+ * Return: char value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
