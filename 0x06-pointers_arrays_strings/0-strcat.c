3fbfa38cf0f2080ee5b703a5fb9164f03ec0b84b
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-24 07:24:42 -0700

    Task 0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concatenate two strings
+ * @dest: the destination
+ * @src: the source
+ * Return: a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Another task to learn about pointers
