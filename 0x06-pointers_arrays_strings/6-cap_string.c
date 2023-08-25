c53d427d2f8d9ba9eef9908c22fc973ba45f1921
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-25 06:00:59 -0700

    Capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,13 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-	bool new_word = 1;
+	int new_word = 1;
+	char separator[] = {32, '\t', '\n', 44, ';', 46,
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	while (s[i])
 	{
-		if (is_separator(s[i]))
+		if (s[i] == separator[i])
 			new_word = 1;
 
 		else if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
