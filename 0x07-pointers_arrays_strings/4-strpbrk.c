e72164355afcc33e7ea59dc97e556ef9233b26df
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-28 15:12:17 -0700

    locates the first occurrence in the string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,4 +9,20 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-jhhhhh
+	unsigned int i = 0, a;
+
+	while (s[i] != '\0')
+	{
+		a = 0;
+		while (accept[a] != '\0')
+		{
+			if (accept[a] == s[i])
+			{
+				return (s + i);
+			}
+			a++;
+		}
+		i++;
+	}
+	return (NULL);
+}
