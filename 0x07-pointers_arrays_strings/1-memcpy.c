71b1148673eba9bff13e6078421c99843dfd807f
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-28 09:09:26 -0700

    Copy memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - COpy src into dest n number of times
+ * @dest: the buffer i am copying into
+ * @src: the source of the bytes
+ * @n: size of the src
+ * Return: pointer to the dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		 dest[i++] = src[i++];
+	}
+	return (dest);
+}
