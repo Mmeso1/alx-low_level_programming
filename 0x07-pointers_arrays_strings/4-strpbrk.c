a7bdaa54e990eb998876ab86bb1b727cf3c8899b
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-28 15:03:37 -0700

    locates the first occurrence in the string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence of a character from @accept in @s
+ * @s: the string to search in
+ * @accept: the set of characters to search for
+ * Return: pointer to the byte in @s that matches
+ * one of the bytes in @accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0, a;
+
+	while (s[i] != '\0')
+	{
+		a = 0;
+		while (accept[a] != '\0')
+		{
+			if (accept[a] == s[i])
+			{
+				return (s + i);
+			}
+			a++;
+		}
+		i++;
+	}
+	return (NULL);
+}
