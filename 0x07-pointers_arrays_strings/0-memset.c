02991163f26722804e3a6511b2ccf2be184bc15d
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-28 08:26:36 -0700

    Set memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,15 @@
 
 /**
  * _memset - fills the first n bytes with b of s
- * @*s: the memeory area being pointed to
+ * @s: the memory area being pointed to
  * @b: the source of the constant
  * @n: the size of bytes
+ * Return: pointer to the input string
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
