52da7781feb59fe47b5b9eae4d54d3ae71457cf0
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-28 15:07:25 -0700

    Delete task 4 swp

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,29 +1,12 @@
 #include "main.h"
 
 /**
- * _strpbrk - locates the first occurrence of a character from @accept in @s
- * @s: the string to search in
- * @accept: the set of characters to search for
- * Return: pointer to the byte in @s that matches
- * one of the bytes in @accept
+ * _strpbrk - 
+ * @s:
+ * @accept:
+ * Return:
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i = 0, a;
-
-	while (s[i] != '\0')
-	{
-		a = 0;
-		while (accept[a] != '\0')
-		{
-			if (accept[a] == s[i])
-			{
-				return (s + i);
-			}
-			a++;
-		}
-		i++;
-	}
-	return (NULL);
-}
+jhhhhh
