fce87a33d188ea5aa5d0127e6d20ccc51bd124d8
Mmeso1 <anisiubammesoma8@gmail.com>
2023-08-30 15:12:13 -0700

    Compare strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare strings to check how identical they are
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 or 1
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (s2 == '*')
+	{
+		if wildcmp(s1, s2 + 1)
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
