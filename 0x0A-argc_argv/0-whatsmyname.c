34c19d7509dd0ae31fd5039a55af93ada8119680
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-02 02:45:32 -0700

    Print name of running program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -print the name of the file running
+ * @argc: arg count
+ * @argv: arg vetor
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
