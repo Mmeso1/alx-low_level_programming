06079f62b3fc8d342cd7539b379ebebd47e247ae
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-02 03:34:58 -0700

    Print product of 2 argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,13 +17,13 @@ int main(int argc, char *argv[])
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 
-		printf("%d", num1 * num2);
+		printf("%d\n", num1 * num2);
 
 		return (0);
 	}
 	else
 	{
-		printf("Error\nn");
+		printf("Error\n");
 		return (1);
 	}
 }
