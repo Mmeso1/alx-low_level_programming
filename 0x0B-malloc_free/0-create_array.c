ad7473a16c812876e39a15b8cbfb2aaf7d210edf
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-05 14:46:41 -0700

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = (char *)malloc((size) * sizeof(char));
+	array = (char *)malloc(size);
 
 	if (array != NULL)
 	{
