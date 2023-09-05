444a14cbb6b33edc8511fa7c985fb0d697146611
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-05 15:54:12 -0700

    Concatenate string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1[len1] != '\0')
 		len1++;
-	while (s1[len2] != '\0')
+	while (s2[len2] != '\0')
 		len2++;
 
 	concatenated = malloc(sizeof(char) * (len1 + len2 + 2));
