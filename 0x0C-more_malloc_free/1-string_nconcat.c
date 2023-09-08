5090dc987cf8588def529a2af869f25b3f115e08
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-08 07:38:09 -0700

    Concatenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int lens1 = strlen(s1);
 	unsigned int lens2 = strlen(s2);
-	char *result = malloc(len1 + n + 1);
+	char *result = malloc(lens1 + n + 1);
 
 	if (s1 == NULL)
 		s1 = " ";
