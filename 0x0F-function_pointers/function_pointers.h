25a1ee0224e2c3ccbc0624833acd54e522eea633
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-13 14:54:54 -0700

    Update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,7 @@
 #ifndef _HEADER_
 #define _HEADER_
 
-void print_name_uppercase(char *name);
-void print_name_as_is(char *name);
+
 void print_name(char *name, void (*f)(char *));
 /**
 void array_iterator(int *array, size_t size, void (*action)(int));
