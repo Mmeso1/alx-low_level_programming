6a581e71fd1759fa14973e9d1c86361a2c6f6905
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2023-09-14 06:00:11 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
-/**
- * main - 
- * @argc:
- * @argv:
- */
-int main(int argc, char *argv[])
-{
-	ud_t ud_obj;
-	int val = 0, i = 0;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;
+    }
 
-	if (argc == 2)
-	{
-		val = atoi(argv[1]);
+    int num_bytes = atoi(argv[1]);
+    if (num_bytes < 0) {
+        printf("Error\n");
+        return 2;
+    }
 
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
+    unsigned char *main_ptr = (unsigned char *)&main;
+    for (int i = 0; i < num_bytes; i++) {
+        printf("%02x", main_ptr[i]);
+        if (i < num_bytes - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
 
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
-	}
-
-	return (0);
+    return 0;
 }
