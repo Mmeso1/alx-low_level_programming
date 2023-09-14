6eedeeff76329243f5e7cfe11558e49e24771fdf
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-14 09:12:28 -0700

    Print numbers in args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print the numbers args
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator != NULL && n != 0)
+	{
+		va_list args;
+		unsigned int i;
+
+		va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d%s", va_arg(args, int), separator);
+
+		}
+		printf("\n");
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_HEADER
 #define VARIADIC_HEADER
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
