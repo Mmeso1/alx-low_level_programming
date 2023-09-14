e1c932e39435d509d6599e2a772af407c8b8533a
Mmeso <117354340+Mmeso1@users.noreply.github.com>
2023-09-14 06:10:16 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Error\n");
-        return 1;
+        return (1);
     }
 
     int num_bytes = atoi(argv[1]);
     if (num_bytes < 0)
     {
         printf("Error\n");
-        return 2;
+        return (2);
     }
 
     unsigned char *main_ptr = (unsigned char *)&main;
@@ -34,9 +34,5 @@ int main(int argc, char *argv[])
     }
     printf("\n");
 
-    return 0;
-}
-
-
-    return 0;
+    return (0);
 }
