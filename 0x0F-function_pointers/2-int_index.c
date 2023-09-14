e9f53a01702c2ea9f39744263430aa66486f52d7
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-14 04:18:27 -0700

    Return index of found integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
-		return (-1);
-
-	for (; i < size; i++)
+	if (array != NULL && size > 0 && cmp != NULL)
 	{
-		if (cmp(array[i]) != 0)
+		if (; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i] != 0))
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
