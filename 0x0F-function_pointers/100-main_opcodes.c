59d9864c024c6477708dd406effd065660b77d25
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-13 22:19:14 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+/**
+ * main - Prints the opcodes of its own main function
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, 1 on invalid argument count,
+ * 2 on negative bytes
+ */
 
-    int num_bytes = atoi(argv[1]);
-    if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    unsigned char *main_ptr = (unsigned char *)&main;
-    for (int i = 0; i < num_bytes; i++) {
-        printf("%02x", main_ptr[i]);
-        if (i < num_bytes - 1) {
-            printf(" ");
-        }
-    }
-    printf("\n");
+	int num_bytes = atoi(argv[1]);
 
-    return 0;
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *main_ptr = (unsigned char *)&main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+		if (i < num_bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
