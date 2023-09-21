7ddacbef1393cbe305c73f2f422b73ab45508afc
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-21 14:40:51 -0700

    Print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->next != NULL)
-			printf("[%d} %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		node_count++;
 		h = h->next;
 	}
