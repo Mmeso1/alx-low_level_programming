1734fe8eee3a845f612fe2266e146dec0945a9e2
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-21 17:31:47 -0700

    Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,16 @@
-global      main
-extern      printf
+section .data
+   hello_message db "Hello, Holberton",10,0
+
+section .text
+   global  main
+   extern  printf
+
 main:
-       mov   edi, format
-       xor   eax, eax
-       call  printf
-       mov eax, 0
-       ret
-format: db `Hello, Holberton\n",0
+   push rbp
+   mov rdi, hello_message
+   call printf
+   pop rbp
+
+   mov rax, 60
+   xor rdi, rdi
+   syscall
