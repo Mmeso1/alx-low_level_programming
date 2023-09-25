fd5b1942f218e4194e4bc609b6c2b6a756e1e70a
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-25 14:33:01 -0700

    Task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (i == index)
-			return (index);
+			return (head);
 		head = head->next;
 		i++;
 	}
