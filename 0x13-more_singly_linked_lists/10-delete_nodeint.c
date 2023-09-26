fd7f7100dcf6ca3ee4d3563ea6929e7fc2712099
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-26 13:34:03 -0700

    Task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = *head;
 		*head = (*head)->next;
 		free(current);
-		return (-1);
+		return (1);
 	}
 	current = *head;
 	previous = NULL;
