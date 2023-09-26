94a9b4fe747348492a7aa0a9f479a8850223e1c0
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-25 23:52:42 -0700

    Task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,6 +5,7 @@
  * @head: pointer to the head node pinter
  * @idx: the index for the insertion
  * @n: the value of the node
+ * Return:  the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
