9ee40afa8420f08fcb666edfc9d8e5523afd05af
Mmeso1 <anisiubammesoma8@gmail.com>
2023-09-28 12:47:40 -0700

    Task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary representation of the argument
+ * @n: the argument
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << 63;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mask > 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
+}
