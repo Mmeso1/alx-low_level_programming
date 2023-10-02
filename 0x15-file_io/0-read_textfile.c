46a59017d5ddd906b0f37936e8094d557c5d76bb
Mmeso1 <anisiubammesoma8@gmail.com>
2023-10-02 07:00:23 -0700

    task on file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the name of the file
+ * @letters: the number of letters to be printed
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file_pointer;
+	char buffer[1024];
+	size_t read_bytes;
+	ssize_t count = 0;
+
+	if (!filename)
+		return (0);
+
+	file_pointer = fopen(filename, "r");
+	if (!file_pointer)
+		return (0);
+
+	while(!feof(file_pointer))
+	{
+		read_bytes = fread(buffer, 1, letters, file_pointer);
+
+		if (read_bytes == 0)
+			break;
+		buffer[read_bytes] = '\0';
+		count += read_bytes;
+		printf("%s", buffer);
+	}
+	fclose(file_pointer);
+	return (count);
+}
