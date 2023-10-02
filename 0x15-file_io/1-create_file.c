7468ecabe1d597a0804dcd0ed2603399d0f4f216
Mmeso1 <anisiubammesoma8@gmail.com>
2023-10-02 10:27:20 -0700

    Update on create file task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int filde;
 	size_t written_byte;
-	
+
 	if (filename == NULL)
 		return (-1);
 
-	filde = open(filename, O_WRONLY | O_CREAT| O_TRUNC, 0600);
+	filde = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (filde < 0)
 		return (-1);
 
