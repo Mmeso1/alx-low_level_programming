a152bccf02fba0fa81d629b967271a57b7002471
Mmeso1 <anisiubammesoma8@gmail.com>
2023-11-13 19:06:36 -0800

    Task 3

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+/**
+ * add - add operation
+ * @a: var a
+ * @b: var b
+ * Return: number result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract operation
+ * @a: var a
+ * @b: var b
+ * Return: number result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply operation
+ * @a: var a
+ * @b: var b
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide operation
+ * @a: var a
+ * @b: var b
+ * Return: dividend
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus operation
+ * @a: var a
+ * @b: var b
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
