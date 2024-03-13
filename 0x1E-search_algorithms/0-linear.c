e9e7f21095d4adda538ad4a49c8c1e052dff9568
Mmeso1 <anisiubammesoma8@gmail.com>
2024-03-13 10:49:12 -0700

    betty fox for task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,8 +8,8 @@
  * @size: the number of elements in array
  * @value: the value to search for
  * Return: first index where value is located
- * 	   If value is not present in array or if array is NULL,
- * 	   your function must return -1
+ * If value is not present in array or if array is NULL,
+ * your function must return -1
  */
 int linear_search(int *array, size_t size, int value)
 {
