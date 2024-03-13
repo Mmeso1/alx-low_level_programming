49e703aacfdbc95afba4c9a49c2a1b40b058696a
Mmeso1 <anisiubammesoma8@gmail.com>
2024-03-13 15:49:38 -0700

    task 1 on binary search done

--- 0x1E-search_algorithms/base.c
+++ 0x1E-search_algorithms/base.c
@@ -11,17 +11,3 @@
  * your function must return -1
  */
 int linear_search(int *array, size_t size, int value)
-{
-	int i = 0;
-
-	if (!array)
-		return (-1);
-
-	for (i = 0; i < (int)size; i++)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
-}
